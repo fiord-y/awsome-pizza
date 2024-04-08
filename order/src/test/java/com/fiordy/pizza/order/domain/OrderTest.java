d8905e1377d512af41dc1fe9ec23c8e3056dcaf8
fiord-y <fiordes.agaj@gmail.com>
2024-04-08 15:39:47 +0200

    fix on comparator impl

--- order/src/test/java/com/fiordy/pizza/order/domain/OrderTest.java
+++ order/src/test/java/com/fiordy/pizza/order/domain/OrderTest.java
@@ -2,17 +2,33 @@ package com.fiordy.pizza.order.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderTest {
 
     @Test
-    void compareTo() {
+    void compareTo_different_priority() {
         var o1 = new Order();
         o1.setType(OrderType.DINE_IN);
 
         var o2 = new Order();
         o2.setType(OrderType.DELIVERY);
+
+        var compared = o1.compareTo(o2);
+        assertEquals(-1, compared);
+    }
+
+    @Test
+    void compareTo_same_priority() {
+        var o1 = new Order();
+        o1.setType(OrderType.DINE_IN);
+        o1.setDueAt(LocalDateTime.now().plusMinutes(30));
+
+        var o2 = new Order();
+        o2.setType(OrderType.DINE_IN);
+        o2.setDueAt(LocalDateTime.now().plusMinutes(100));
 
         var compared = o1.compareTo(o2);
         assertEquals(-1, compared);
